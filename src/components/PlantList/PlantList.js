bbb565a77520c99beb48734cc45fc7d14c451d10
Forrest Nelson <forrestnnelson@gmail.com>
2023-08-23 23:22:51 -0500

    GET and POST working, added DELETE button to work on next

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const reduxState = useSelector(store => store.plantList);
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
-    }, []); 
+        dispatch({ type: 'GET_PLANTS' })
+    }, []);
+
+    console.log("reduxState is", reduxState)
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {reduxState.map((plant, index) => (
+                <div key={index} className='plant-item'>
+                    <p>{plant.name}</p>
+                    <button>Delete</button>
+                    <hr />
+                </div>
+            ))}
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'
+import { takeLatest, put } from 'redux-saga/effects'
+import axios from 'axios';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+// plantList reducer - listens for specific action types and has an initial state that it updates
+const plantList = (state = [], action) => {
   switch (action.type) {
+    case 'GET_PLANT_DATA':
+      return action.payload;
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return [...state, action.payload]
     default:
       return state;
   }
 };
 
+function* addPlant(action) {
+  // console.log("payload for POST:", action.payload)
+  yield axios.post('/api/plant', action.payload)
+}
+
+function* getPlants(action) {
+  // addPlantsData works like response, addPlantsData.data is like response.data
+  const addPlantsData = yield axios.get('/api/plant')
+  console.log("getting plants:", addPlantsData.data);
+  // after get, this works like .then, and dispatches (put) action which is heard in the plantList reducer  
+  yield put({ type: 'GET_PLANT_DATA', payload: addPlantsData.data })
+}
+
+function* rootSaga() {
+  yield takeLatest('GET_PLANTS', getPlants)
+  yield takeLatest('NEW_PLANT', addPlant)
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({
+    plantList
+  }),
+  applyMiddleware(
+    logger,
+    sagaMiddleware
+  )
 );
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
